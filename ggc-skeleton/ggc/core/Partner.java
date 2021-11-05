a427a9563bc1b26332e85f2c90d5ed39784623ad
Guilherme Carabalone <guilherme.caraba@gmail.com>
2021-11-05 14:43:45 -0300

    fixed bugs

--- ggc-skeleton/ggc/core/Partner.java
+++ ggc-skeleton/ggc/core/Partner.java
@@ -34,7 +34,7 @@ public class Partner implements Comparable<Partner>, Serializable{
 
 
     public int compareTo(Partner p){
-        return _id.compareTo(p.getId());
+        return _id.toLowerCase().compareTo(p.getId().toLowerCase());
     }
 
     
